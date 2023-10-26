9f0532847d4985a96823c69c7bfccaa999eafc98
Lia Auliya <146051085+liaauliyasi01@users.noreply.github.com>
2023-10-26 13:53:16 +0700

    Tugas Pekan 4 ddp
    Lia Auliya_Sistem Informasi01_0110123275

---
+++ pekan4_aritmatika.py
@@ -0,0 +1,22 @@
+bil1= 12
+bil2= 13
+
+# operator penjumlahan (+)
+hasil = bil1 + bil2
+
+# operator pengurangan (-)
+hasil = bil1 - bil2
+
+# operator perkalian (*)
+hasil = bil1 * bil2
+
+# operator pembagian (/)
+hasil = bil1 / bil2
+
+# operator modulus (%)
+hasil = bil1 % bil2
+
+# operator perpangkatan (**)
+hasil = bil1 ** bil2
+
+# operator floor difision 
