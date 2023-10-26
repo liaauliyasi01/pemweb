704f64debf3aa42563dcf386d941bfdb4a2e8a12
liaauliyasi01 <146051085+liaauliyasi01@users.noreply.github.com>
2023-10-26 13:30:08 +0700

    Tugas Pekan 5 Pemweb
    Lia Auliya_Sistem Infromasi01_0110123275

---
+++ variabelvalue.py
@@ -0,0 +1,10 @@
+# Soal No.1
+
+mobil = ["mobil","sport utility vehicle","3996","kuning","4"]
+print(mobil)
+mobil.append("9.000.000.000")
+print(mobil)
+mobil.append("Lamborghini Urus")
+print(mobil)
+mobil.insert(2,"Lamborghini Urus")
+
